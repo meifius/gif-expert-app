7b4a1c3a5958a067536363e5f7df635c8ed0912f
unknown <mejia.alan.r@gmail.com>
2020-08-05 11:15:19 -0300

    Build folder has created to deploy it in GitHub

---
+++ docs/precache-manifest.61587f8a892e2a2fd0a0df6d5b105ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c786bbac431fb6ebf41183259b1b579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee8d5445f0637ff714ea",
+    "url": "/static/css/main.04f92175.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8d5445f0637ff714ea",
+    "url": "/static/js/main.7bd44544.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
